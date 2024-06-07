a333ed25b3b5cd3320bf4830d7db2db55314deba
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2024-06-07 14:27:20 +0700

    fix sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -3,8 +3,8 @@
     <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
         <!--begin::Logo image-->
         <a href="{{ route('admin.index') }}">
-            <img alt="Logo" src="/assets/images/KANREG3-BKN-WHITE.png" class="h-50px app-sidebar-logo-default" />
-            <img alt="Logo" src="/assets/images/logo-circle.png" class="h-35px app-sidebar-logo-minimize" />
+            <img alt="Logo" src="{{ asset('assets/images/KANREG3-BKN-WHITE.png') }}" class="h-50px app-sidebar-logo-default" />
+            <img alt="Logo" src="{{ asset('assets/images/logo-circle.png') }}" class="h-35px app-sidebar-logo-minimize" />
         </a>
         <div id="kt_app_sidebar_toggle" class="app-sidebar-toggle btn btn-icon btn-shadow btn-sm btn-color-muted btn-active-color-primary h-30px w-30px position-absolute top-50 start-100 translate-middle rotate" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="app-sidebar-minimize">
             <i class="ki-duotone ki-black-left-line fs-3 rotate-180">
