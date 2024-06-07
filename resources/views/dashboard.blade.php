668b1f82fe0e488da3a235363e514136bb278e21
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2024-06-07 11:00:53 +0700

    fix(admin): Fix dashboard jika periode belum ada ang aktif

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,71 +32,77 @@
 @endsection
 
 @section('content')
-    <!--begin::Row-->
-    <div class="card h-md-100">
-        <div class="card-header position-relative py-0 border-bottom-1">
-            <h3 class="card-title text-gray-800 fw-bold">{{ $datas->nama_periode }}</h3>
-            <ul class="nav nav-stretch nav-pills nav-pills-custom d-flex mt-4">
-                @forelse ($datas->location_available as $locAvail)
-                    <li class="nav-item p-0 ms-0">
-                        <a class="nav-link btn btn-color-gray-400 flex-center px-3 {{ $loop->iteration == 1 ? 'active' : '' }}"
-                            data-bs-toggle="tab" href="#{{ $locAvail->id }}">
-                            <span class="nav-text fw-semibold fs-4 mb-3">{{ $locAvail->lokasi->name }}</span>
-                            <span
-                                class="bullet-custom position-absolute z-index-2 w-100 h-1px top-100 bottom-n100 bg-primary rounded"></span>
-                        </a>
-                    </li>
-                @empty
-                @endforelse
-            </ul>
-        </div>
-        <div class="card-body pb-0">
-            <div class="tab-content">
-                @forelse ($datas->location_available as $locAvail)
-                    <div class="tab-pane {{ $loop->iteration == 1 ? 'active' : '' }}" id="{{ $locAvail->id }}"
-                        role="tabpanel" aria-labelledby="day-tab">
-                        <div class="table-responsive pb-10">
-                            @forelse ($locAvail->lokasi->jadwal as $jadwal)
-                                @php
-                                    $schedules = \App\Models\Schedule::query()
-                                        ->with('peserta')
-                                        ->wherePeriodId($jadwal->period_id)
-                                        ->whereLocationId($jadwal->location_id)
-                                        ->where('tanggal', 'like', '%'.date('Y-m-d', strtotime($jadwal->tanggal)).'%')
-                                        ->orderBy('nama_sesi')
-                                        ->get();
-                                @endphp
-                                <div class="row">
-                                    <div class="col-12 mb-5">
-                                        <div class="border border-3 border-primary rounded">
-                                            <div class="text-center bg-primary text-white w-100 p-3">
-                                                <span
-                                                    class="fs-3 fw-bold">{{ formatTanggalIndonesia($jadwal->tanggal) }}</span>
-                                            </div>
+    @if ($datas)
+        <div class="card h-md-100">
+            <div class="card-header position-relative py-0 border-bottom-1">
+                <h3 class="card-title text-gray-800 fw-bold">{{ $datas->nama_periode }}</h3>
+                <ul class="nav nav-stretch nav-pills nav-pills-custom d-flex mt-4">
+                    @forelse ($datas->location_available as $locAvail)
+                        <li class="nav-item p-0 ms-0">
+                            <a class="nav-link btn btn-color-gray-400 flex-center px-3 {{ $loop->iteration == 1 ? 'active' : '' }}"
+                                data-bs-toggle="tab" href="#{{ $locAvail->id }}">
+                                <span class="nav-text fw-semibold fs-4 mb-3">{{ $locAvail->lokasi->name }}</span>
+                                <span
+                                    class="bullet-custom position-absolute z-index-2 w-100 h-1px top-100 bottom-n100 bg-primary rounded"></span>
+                            </a>
+                        </li>
+                    @empty
+                    @endforelse
+                </ul>
+            </div>
+            <div class="card-body pb-0">
+                <div class="tab-content">
+                    @forelse ($datas->location_available as $locAvail)
+                        <div class="tab-pane {{ $loop->iteration == 1 ? 'active' : '' }}" id="{{ $locAvail->id }}"
+                            role="tabpanel" aria-labelledby="day-tab">
+                            <div class="table-responsive pb-10">
+                                @forelse ($locAvail->lokasi->jadwal as $jadwal)
+                                    @php
+                                        $schedules = \App\Models\Schedule::query()
+                                            ->with('peserta')
+                                            ->wherePeriodId($jadwal->period_id)
+                                            ->whereLocationId($jadwal->location_id)
+                                            ->where(
+                                                'tanggal',
+                                                'like',
+                                                '%' . date('Y-m-d', strtotime($jadwal->tanggal)) . '%',
+                                            )
+                                            ->orderBy('nama_sesi')
+                                            ->get();
+                                    @endphp
+                                    <div class="row">
+                                        <div class="col-12 mb-5">
+                                            <div class="border border-3 border-primary rounded">
+                                                <div class="text-center bg-primary text-white w-100 p-3">
+                                                    <span
+                                                        class="fs-3 fw-bold">{{ formatTanggalIndonesia($jadwal->tanggal) }}</span>
+                                                </div>
 
-                                            <div class="p-5">
-                                                <div class="row">
-                                                    @forelse ($schedules as $schedule)
-                                                        <div class="col-12 col-md-3">
-                                                            <div class="card shadow">
-                                                                <div class="d-flex justify-content-center border-bottom pt-5 pb-4">
-                                                                    <span class="card-title fw-bolder fs-3">
-                                                                        {{ $schedule->nama_sesi }}
-                                                                       </span>
-                                                                </div>
-                                                                <div class="card-body text-center p-5">
-                                                                    <div class="d-flex flex-column align-items-center">
-                                                                        <div class="fw-bold d-flex flex-column gap-0"
-                                                                            style="font-size: 65px;">
-                                                                            {{ $schedule->peserta->count() . '/' . $schedule->kuota }}
-                                                                            <span style="margin-top:-10px;"
-                                                                                class="fs-4">Peserta</span>
-                                                                        </div>
-
-                                                                    </div>
-                                                                </div>
-                                                                <div class="card-footer {{ $schedule->kuota <= $schedule->peserta->count() ? 'bg-danger' : 'bg-primary' }} p-0">
+                                                <div class="p-5">
+                                                    <div class="row">
+                                                        @forelse ($schedules as $schedule)
+                                                            <div class="col-12 col-md-3">
+                                                                <div class="card shadow">
                                                                     <div
+                                                                        class="d-flex justify-content-center border-bottom pt-5 pb-4">
+                                                                        <span class="card-title fw-bolder fs-3">
+                                                                            {{ $schedule->nama_sesi }}
+                                                                        </span>
+                                                                    </div>
+                                                                    <div class="card-body text-center p-5">
+                                                                        <div class="d-flex flex-column align-items-center">
+                                                                            <div class="fw-bold d-flex flex-column gap-0"
+                                                                                style="font-size: 65px;">
+                                                                                {{ $schedule->peserta->count() . '/' . $schedule->kuota }}
+                                                                                <span style="margin-top:-10px;"
+                                                                                    class="fs-4">Peserta</span>
+                                                                            </div>
+
+                                                                        </div>
+                                                                    </div>
+                                                                    <div
+                                                                        class="card-footer {{ $schedule->kuota <= $schedule->peserta->count() ? 'bg-danger' : 'bg-primary' }} p-0">
+                                                                        <div
                                                                             class="d-flex justify-content-center align-items-center gap-3 text-white p-5">
                                                                             <i
                                                                                 class="bi bi-{{ $schedule->kuota <= $schedule->peserta->count() ? 'x-lg' : 'check-circle' }} text-white"></i>
@@ -104,61 +110,94 @@
                                                                                 {{ $schedule->kuota <= $schedule->peserta->count() ? 'Penuh' : 'Tersedia' }}
                                                                             </span>
                                                                         </div>
+                                                                    </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
-                                                    @empty
-                                                    @endforelse
+                                                        @empty
+                                                        @endforelse
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            @empty
-                                <div
-                                    class="alert alert-dismissible bg-light-danger d-flex flex-center flex-column py-10 px-10 px-lg-20">
-                                    <!--begin::Icon-->
-                                    <i class="ki-duotone ki-information-5 fs-5tx text-danger mb-5"><span
-                                            class="path1"></span><span class="path2"></span><span
-                                            class="path3"></span></i>
-                                    <!--end::Icon-->
+                                @empty
+                                    <div
+                                        class="alert alert-dismissible bg-light-danger d-flex flex-center flex-column py-10 px-10 px-lg-20">
+                                        <!--begin::Icon-->
+                                        <i class="ki-duotone ki-information-5 fs-5tx text-danger mb-5"><span
+                                                class="path1"></span><span class="path2"></span><span
+                                                class="path3"></span></i>
+                                        <!--end::Icon-->
 
-                                    <!--begin::Wrapper-->
-                                    <div class="text-center">
-                                        <!--begin::Title-->
-                                        <h1 class="fw-bold mb-5 text-danger">Belum tersedia</h1>
-                                        <!--end::Title-->
+                                        <!--begin::Wrapper-->
+                                        <div class="text-center">
+                                            <!--begin::Title-->
+                                            <h1 class="fw-bold mb-5 text-danger">Belum tersedia</h1>
+                                            <!--end::Title-->
 
-                                        <!--begin::Separator-->
-                                        <div class="separator separator-dashed border-danger opacity-25 mb-5"></div>
-                                        <!--end::Separator-->
+                                            <!--begin::Separator-->
+                                            <div class="separator separator-dashed border-danger opacity-25 mb-5"></div>
+                                            <!--end::Separator-->
 
-                                        <!--begin::Content-->
-                                        <div class="mb-9 text-gray-900">
-                                            Belum ada jadwal simulasi CAT yang tersedia.
+                                            <!--begin::Content-->
+                                            <div class="mb-9 text-gray-900">
+                                                Belum ada jadwal simulasi CAT yang tersedia.
+                                            </div>
+                                            <!--end::Content-->
+
+                                            <!--begin::Buttons-->
+                                            <div class="d-flex flex-center flex-wrap">
+                                                <a href="{{ route('admin.jadwal.index') }}"
+                                                    class="btn btn-danger m-2">Tambah Jadwal</a>
+                                            </div>
+                                            <!--end::Buttons-->
                                         </div>
-                                        <!--end::Content-->
-
-                                        <!--begin::Buttons-->
-                                        <div class="d-flex flex-center flex-wrap">
-                                            <a href="{{ route('admin.jadwal.index') }}" class="btn btn-danger m-2">Tambah Jadwal</a>
-                                        </div>
-                                        <!--end::Buttons-->
+                                        <!--end::Wrapper-->
                                     </div>
-                                    <!--end::Wrapper-->
-                                </div>
-                            @endforelse
+                                @endforelse
+                            </div>
                         </div>
-                    </div>
-                @empty
-                @endif
+                    @empty
+                    @endif
+                </div>
+                <!--end::Tab content-->
             </div>
-            <!--end::Tab content-->
+            <!--end::Card body-->
         </div>
-        <!--end::Card body-->
-    </div>
-    {{-- <div class="d-flex justify-content-center border border-gray-500 rounded p-10">
-        <h1>Selamat datang di Aplikasi Jadwal Simulasi CAT Kanreg III BKN Bandung</h1>
-    </div> --}}
-    <!--end::Row-->
+    @else
+        <div class="card h-md-100">
+            <div class="card-body">
+                <div class="alert alert-dismissible bg-light-danger d-flex flex-center flex-column py-10 px-10 px-lg-20">
+                    <!--begin::Icon-->
+                    <i class="ki-duotone ki-information-5 fs-5tx text-danger mb-5"><span class="path1"></span><span
+                            class="path2"></span><span class="path3"></span></i>
+                    <!--end::Icon-->
+
+                    <!--begin::Wrapper-->
+                    <div class="text-center">
+                        <!--begin::Title-->
+                        <h1 class="fw-bold mb-5 text-danger">Belum tersedia</h1>
+                        <!--end::Title-->
+
+                        <!--begin::Separator-->
+                        <div class="separator separator-dashed border-danger opacity-25 mb-5"></div>
+                        <!--end::Separator-->
+
+                        <!--begin::Content-->
+                        <div class="mb-9 text-gray-900">
+                            Belum ada periode Jadwal Simulasi CAT yang aktif.
+                        </div>
+                        <!--end::Content-->
+
+                        <!--begin::Buttons-->
+                        <div class="d-flex flex-center flex-wrap">
+                            <a href="{{ route('admin.master.periode.index') }}" class="btn btn-danger m-2">Tambah Periode</a>
+                        </div>
+                        <!--end::Buttons-->
+                    </div>
+                    <!--end::Wrapper-->
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
